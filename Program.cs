2ff408eb75d7480278b5bcf900c5a9cff2d4cdd8
gamer99122 <gamer99122@hotmail.com.tw>
2024-01-18 14:21:11 +0800

    Add Main

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace IC20Analysis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
